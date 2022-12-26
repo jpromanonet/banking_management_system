2dabb7e9542977de18079696a83616fd90bc078e
jpromanonet <programming@jpromano.net>
2022-12-26 16:41:10 -0300

    Almost done

---
+++ banking_management_system/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	// User menu
+	cout << "1. Open Account \n";
+	cout << "2. Deposite money \n";
+
+	//getch();
+	return 0;
+}
