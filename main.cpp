ec8b52e8af4404e733864bb6db3374a45c9152e6
jpromanonet <programming@jpromano.net>
2022-12-26 18:20:23 -0300

    Almost done

--- main.cpp
+++ main.cpp
@@ -3,8 +3,39 @@
 #include <iostream>
 
 using namespace std;
+// Bank class (functions)
+class bank 
+{
+	// Declaring variables
+	char name[100];
+	char address[100];
+	char account;
 
-int main() {
+	// Public functions
+	public:
+		void open_account();
+		void deposite_money();
+		void withdraw_money();
+		void display_account();
+};
+
+// Functions
+void bank::open_account() 
+{
+	cout << "Enter your full name: ";
+	cin.ignore();
+	cin.getline(name, 100);
+	cout << "Enter your address: ";
+	cin.ignore();
+	cin.getline(address, 100);
+	cout << "Type of account | savings (S) or current (C): ";
+	cin >> account;
+	cout << "Enter deposit amount: ";
+}
+
+// Main functions
+int main() 
+{
 
 	// User menu
 	cout << "1. Open Account \n";
